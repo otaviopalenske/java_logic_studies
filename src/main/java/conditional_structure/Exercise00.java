3a7edc2e56e41ed642489e0565c98a96a1be2466
Otavio Palenske <otaviopalenske@gmail.com>
2026-05-28 19:26:42 -0300

    feat: conditional structure exercise

---
+++ src/main/java/conditional_structure/Exercise00.java
@@ -0,0 +1,28 @@
+package conditional_structure;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise00 {
+
+    static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int hour;
+
+        System.out.println("What hour is it?");
+        hour = sc.nextInt();
+
+        if (hour < 12) {
+            System.out.println("Good morning");
+        } else if (hour < 18) {
+            System.out.println("Good Afternoon");
+        } else {
+            System.out.println("Good Night");
+        }
+
+        sc.close();
+
+    }
+}
