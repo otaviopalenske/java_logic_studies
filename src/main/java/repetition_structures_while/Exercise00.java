5afe115f60b9340c3687430344713ac14ba04679
Otavio Palenske <otaviopalenske@gmail.com>
2026-05-31 23:31:32 -0300

    feat: repetitions structure exercises

--- src/main/java/repetition_structures_while/Exercise00.java
+++ src/main/java/repetition_structures_while/Exercise00.java
@@ -1,19 +1,22 @@
-package repetition_structures;
+package repetition_structures_while;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Exercise01 {
+public class Exercise00 {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
         int x = scanner.nextInt();
+        int sum = 0;
 
         while (x != 0){
+            sum += x;
             x = scanner.nextInt();
         }
+        System.out.println("Sum = " + sum);
         scanner.close();
     }
 }
